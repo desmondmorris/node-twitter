89627f3dbe312984e29a227fbe15ee91ac07677e
Dara Oladosu <daradosu@gmail.com>
2017-08-24 20:13:03 +0100

    update tests

--- test/twitter.js
+++ test/twitter.js
@@ -16,6 +16,7 @@ describe('Twitter', function() {
         access_token_key: null,
         access_token_secret: null,
         bearer_token: null,
+        language: null,
         rest_base: 'https://api.twitter.com/1.1',
         stream_base: 'https://stream.twitter.com/1.1',
         user_stream_base: 'https://userstream.twitter.com/1.1',
